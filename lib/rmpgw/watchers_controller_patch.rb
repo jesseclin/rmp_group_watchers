045f315fdb7e03e7238b27540d6f72f9cef0243e
jesseclin <jesse.c.lin@outlook.com>
2019-05-03 12:06:55 +0800

    Add Redmine 3.4.x Support

--- lib/rmpgw/watchers_controller_patch.rb
+++ lib/rmpgw/watchers_controller_patch.rb
@@ -16,10 +16,25 @@ module Rmpgw
 
     module InstanceMethods
       def autocomplete_for_user_with_rmpgw
+         scope = nil
+         if @project.present?
+            scope = @project.users
+         else 
+            scope = User.all.limit(100)
+         end
+         users = scope.active.visible.sorted.like(params[:q]).to_a
+         if @project.present?
+            scope = Group.where(id: @project.memberships.joins(:principal)
+                                            .where(:users => {:type => 'Group', :status => Principal::STATUS_ACTIVE}).select(:user_id))
+         else 
+            scope = Group.all.limit(100)
+         end
+         groups = scope.active.visible.order(:lastname).where(type: 'Group').like(params[:q]).limit(100).to_a
+
         if params[:object_type].blank? || params[:object_type] == 'issue'
-          @users = Group.order(:lastname).where(type: 'Group').like(params[:q]).limit(100).to_a + User.active.sorted.like(params[:q]).limit(100).to_a
-          if @watched
-            @users -= @watched.watcher_users
+          @users = groups + users
+          if @watchables.present?
+            @users -= @watchables.first.watcher_users
           end
           render layout: false
         else
@@ -30,19 +45,30 @@ module Rmpgw
       def append_with_rmpgw
         if params[:watcher].is_a?(Hash)
           user_ids = params[:watcher][:user_ids] || [params[:watcher][:user_id]] || []
-          @users = User.joins("LEFT JOIN groups_users on groups_users.user_id = #{User.table_name}.id").active.where("groups_users.group_id in (:user_ids) or #{User.table_name}.id in (:user_ids)", user_ids: user_ids + [0]).uniq.sorted
+          groups = User.joins("LEFT JOIN groups_users on groups_users.user_id = #{User.table_name}.id").active
+              .where("groups_users.group_id in (:user_ids)", user_ids: user_ids + [0])
+          users = User.joins("LEFT JOIN groups_users on groups_users.user_id = #{User.table_name}.id").active
+              .where("#{User.table_name}.id in (:user_ids)", user_ids: user_ids + [0])
+          @users = (groups + users).uniq.sort
         end
       end
 
       def create_with_rmpgw
-        if @watched.is_a?(Issue)
+        logger.debug "TEST"
+        if @watchables.first.class.name.underscore == 'issue'
           if params[:watcher].is_a?(Hash)
             user_ids = (params[:watcher][:user_ids] || params[:watcher][:user_id])
           else
             user_ids = params[:user_id]
           end
           params[:watcher] = {}
-          params[:watcher][:user_id] = User.joins("LEFT JOIN groups_users on groups_users.user_id = #{User.table_name}.id").active.where("groups_users.group_id in (:user_ids) or #{User.table_name}.id in (:user_ids)", user_ids: user_ids + [0]).uniq.sorted.map(&:id)
+          unless user_ids.nil?
+            groups = User.joins("LEFT JOIN groups_users on groups_users.user_id = #{User.table_name}.id").active
+                         .where("groups_users.group_id in (:user_ids)", user_ids: user_ids + [0])
+            users = User.joins("LEFT JOIN groups_users on groups_users.user_id = #{User.table_name}.id").active
+                         .where("#{User.table_name}.id in (:user_ids)", user_ids: user_ids + [0])
+            params[:watcher][:user_id] = (groups + users).uniq.sort.map(&:id)
+          end
         end
 
         create_without_rmpgw
